bd001ebc9a40b1f611147158fd60897b3cd051cb
Benfang Wang <wangbenfang2016@outlook.com>
2019-03-27 19:45:30 -0400

    Update pimp.py

--- pimp.py
+++ pimp.py
@@ -1,3 +1,10 @@
+import playground
+from .protocol import StackingProtocolFactory
+
+pimpConnector = playground.Connector(protocolStack=(PIMPServerProtocol(),PIMPClientProtocol()))
+playground.setConnector("pimp", pimpConnector)
+playground.setConnector("lab1_GoldenNuggetNetSec2019", pimpConnector)
+
 ##====================================================================
 # Assignment: Lab 1 Milestone 1 - Handshake - pimp file
 # Team: GoldenNugget
