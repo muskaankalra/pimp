fc971006c3b8d3df8d43f0367debc13d2783c6c6
Benfang Wang <wangbenfang2016@outlook.com>
2019-03-27 20:51:39 -0400

    Update pimp.py

--- pimp.py
+++ pimp.py
@@ -436,7 +436,7 @@ if __name__=="__main__":
     EnablePresetLogging(PRESET_DEBUG)
     
     if mode.lower() == "server":
-        coro = playground.create_server(lambda: PIMPServerProtocol(), port=103, family=stack)
+        coro = playground.create_server(lambda: PIMPServerProtocol(), port=104, family=stack)
         server = loop.run_until_complete(coro)
         print("Pimp Server Started at {}".format(server.sockets[0].gethostname()))
         loop.run_forever()
@@ -447,7 +447,7 @@ if __name__=="__main__":
         remoteAddress = mode
         coro = playground.create_connection(lambda: PIMPClientProtocol(), 
             host=remoteAddress, 
-            port=103,
+            port=104,
             family=stack)
         transport, protocol = loop.run_until_complete(coro)
         print("Pimp Client Connected. Starting UI t:{}. p:{}".format(transport, protocol))
