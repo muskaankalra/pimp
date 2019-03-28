b65ed92a0af1f3abf633c574235a5826055d00bd
Benfang Wang <wangbenfang2016@outlook.com>
2019-03-27 23:11:29 -0400

    Update pimp.py

--- pimp.py
+++ pimp.py
@@ -325,13 +325,13 @@ class PIMPClientProtocol(StackingProtocol):
     synPacket = PIMPPacket.SynPacket(self.seqNum)
     #logging
     #self.logger.info('Sending SYN packet with Seq Number' + str(self.seqNum))
-    print('Sending SYN packet with Seq Number' + str(self.seqNo))
+    print('Sending SYN packet with Seq Number' + str(self.seqNum))
     transport.write(synPacket.__serialize__())
     print("Sending SYN packet with Seq Number")
   
   def sendAck(self, transport):
     AckPacket = PIMPPacket.AckPacket(self.client_seqNum)
-    print('Sending ACK packet with Ack Number' + str(self.client_seqNum))
+    print('Sending ACK packet with Ack Number' + str(self.client_seqNo))
     #self.logger.info('Sending ACK packet with Ack Number' + str(self.client_seqNum))
     transport.write(AckPacket.__serialize__())
   
@@ -438,7 +438,7 @@ if __name__=="__main__":
     EnablePresetLogging(PRESET_DEBUG)
     
     if mode.lower() == "server":
-        coro = playground.create_server(lambda: PIMPServerProtocol(), port=207, family=stack)
+        coro = playground.create_server(lambda: PIMPServerProtocol(), port=107, family=stack)
         server = loop.run_until_complete(coro)
         print("Pimp Server Started at {}".format(server.sockets[0].gethostname()))
         loop.run_forever()
@@ -449,7 +449,7 @@ if __name__=="__main__":
         remoteAddress = mode
         coro = playground.create_connection(lambda: PIMPClientProtocol(), 
             host=remoteAddress, 
-            port=207,
+            port=107,
             family=stack)
         transport, protocol = loop.run_until_complete(coro)
         print("Pimp Client Connected. Starting UI t:{}. p:{}".format(transport, protocol))
