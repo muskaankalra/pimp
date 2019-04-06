5950807c0390f19aa358fb307c6b557be8bc2c34
Benfang Wang <wangbenfang2016@outlook.com>
2019-04-06 00:18:07 -0400

    Update powerball_server.py

--- powerball/powerball_server.py
+++ powerball/powerball_server.py
@@ -1,7 +1,8 @@
 import asyncio
 import playground
+from Homepage import GoldenNugget 
 
-class EscapeServerClientProtocol(asyncio.Protocol):
+class HomepageServerProtocol(asyncio.Protocol):
 
     def connection_made(self, transport):
         peername = transport.get_extra_info('peername')
@@ -9,25 +10,21 @@ class EscapeServerClientProtocol(asyncio.Protocol):
 
         self.transport = transport
 
-        self.room = EscapeRoom()
-        self.room.start()
+        self.homepage = GoldenNugget()
+        
+        self.transport.write(self.homepage.welcome_narratives())
+        
     def data_received(self, data):
         string = data.decode()
         string = string[:-1]
-        output = self.room.command(string)
+        output = self.homepage.input(string)
         self.transport.write(output.encode())
 
-        status = self.room.status()
-        if status == "dead":
-            self.transport.write(("\r\n"+status).encode())
-        elif status == "escaped":
-            if string == "open door":
-                self.transport.write(("\r\n"+status).encode())
 
 loop = asyncio.get_event_loop()
 
 # Each client connection will create a new protocol instance
-coro = playground.create_server(EscapeServerClientProtocol, '20191.10.20.30', 62261)
+coro = playground.create_server(HomepageServerProtocol, '20191.10.20.30', 6261)
 
 server = loop.run_until_complete(coro)
 
