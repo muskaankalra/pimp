e1adfdd32ded4fe420f9c0e5ab2d2dfed543f737
Benfang Wang <wangbenfang2016@outlook.com>
2019-04-08 17:59:19 -0400

    Update Homepage.py

--- powerball/Homepage.py
+++ powerball/Homepage.py
@@ -1,19 +1,23 @@
-class GoldenNugget():
+from powerball import PowerBall
+
+class Homepage():
   def __init__(self):
     	status = 0 # the game's initial status is 0, 0 means the game is still on the homepage
   
   def input(self,string):
+    #if self.status === 0, choose Service
     if self.status == 0:
           if string >="1" and string <= "6":
-            self.response = "selected:"+ string
+            self.choose_game(sring)
           else:
             self.response = "Improper input."
     elif self.status == 1:
-          self.response = "powerball!!"
-        	#ClientPowerBall.input(string)
+        	self.responese = self.powerball.input(string)
           #self.response = ClientPowerBall.response
     else:
         	self.responese = "Error!!!"
+
+    return self.response
         
   
   def welcome_narratives(self):
@@ -40,3 +44,26 @@ class GoldenNugget():
   		Note: You can choose the menu by enter a single digit that is corresponding to the menu."
       """
       return output 
+      
+  def choose_game(self, userInput):
+    output = ""
+    if userInput == "1":
+        self.status = 1
+        self.powerball = Powerball()
+        output = self.powerball.start()
+    """
+    elif userInput == "2":
+
+        #call roulette
+    elif userInput == "3":
+        #call deal or no deal 
+    elif userInput == "4":
+        # career services
+    elif userInput == "5":
+      #call about us    
+    elif userInput == "6":
+      #call careers
+    """
+    else:
+      output  = "Please enter a single digit of your choice."
+    return output
