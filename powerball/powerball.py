7e89ddf29be23c62676e34d27f5413da17407d55
Benfang Wang <wangbenfang2016@outlook.com>
2019-04-09 09:27:29 -0400

    Update powerball.py

--- powerball/powerball.py
+++ powerball/powerball.py
@@ -18,21 +18,23 @@ class PowerBall:
 	def input(self,choice):
 
 		#string = "\nSelect from the following options:\n1. Pick your numbers\n2. Game Rules\n3. Claim your prize!\n4. Return to Homepage\n"
+		response = ""
+		print("benbenfangfang")
 		if self.status == 0:
 			if choice == "1":
 				self.status = 1
 				response = "Number of tickets you want to buy: "
-				return response
+	
 				
 			elif choice == "2":
 				self.status = 2
 				response = "EACH GAME IS WORTH 10 BITPOINTS\n1. Select five numbers from 1 to 69 or you can also choose a 'Randomly Generated Ticket' that gives you 5 randonly generated numbers\n2. Every Monday and Thursday the PowerBall rolls and 5 random winning numbers are displayed on our home page\n3. If 3 or more of your ticket numbers match with the winning numbers on the PowerBall, you win according to the Prizes listed\n4. To claim your prize, go to our home page and choose the 'Claim Prize' option"
-				return response
+	
 				
 			elif choice == "3":
 				self.status = 3
 				response = "Enter your numbers seperated by a comma: "
-				return response
+		
 
 			elif choice == "4":
 				quit = True
@@ -40,7 +42,6 @@ class PowerBall:
 				response = "Improper input."
 		elif self.status == 1:
 			response = "Do you want to choose your numbers? (Y)es or (N)o: "
-			return response
 
 			"""
 			if (s == "Yes") or (s == "Y") or (s == "yes") or (s == "y"): 
