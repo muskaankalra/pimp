ad61d361d5c8bdd187d18c10019134cb1fb71e21
Benfang Wang <wangbenfang2016@outlook.com>
2019-04-09 09:39:39 -0400

    Update powerball.py

--- powerball/powerball.py
+++ powerball/powerball.py
@@ -19,15 +19,12 @@ class PowerBall:
 
 		#string = "\nSelect from the following options:\n1. Pick your numbers\n2. Game Rules\n3. Claim your prize!\n4. Return to Homepage\n"
 		response = ""
-		print("benbenfangfang")
-		print(choice)
+
 		if self.status == 0:
 			if choice == "1":
 				self.status = 1
 				response = "Number of tickets you want to buy: "
-				print("benbenfan")
-	
-				
+		
 			elif choice == "2":
 				self.status = 2
 				response = "EACH GAME IS WORTH 10 BITPOINTS\n1. Select five numbers from 1 to 69 or you can also choose a 'Randomly Generated Ticket' that gives you 5 randonly generated numbers\n2. Every Monday and Thursday the PowerBall rolls and 5 random winning numbers are displayed on our home page\n3. If 3 or more of your ticket numbers match with the winning numbers on the PowerBall, you win according to the Prizes listed\n4. To claim your prize, go to our home page and choose the 'Claim Prize' option"
