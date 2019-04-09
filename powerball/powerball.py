407acd02ef20679f95f30cd8932bb907c83a2473
Benfang Wang <wangbenfang2016@outlook.com>
2019-04-09 11:58:51 -0400

    Update powerball.py

--- powerball/powerball.py
+++ powerball/powerball.py
@@ -7,10 +7,11 @@ class PowerBall:
 		self.lottery = lottery
 		self.quit = False
 		self.status = 0  # 0 means just enter into the homepage 
+		self.num = 0  # num is the Number of tickets client want to buy
 
 	def getquit(self):
 		return self.quit
-		
+
 	def GenerateRandom(self, num):
 		randlist = []
 		for i in range(0, num):
@@ -30,7 +31,7 @@ class PowerBall:
 		
 			elif choice == "2":
 				response = "EACH GAME IS WORTH 10 BITPOINTS\n1. Select five numbers from 1 to 69 or you can also choose a 'Randomly Generated Ticket' that gives you 5 randonly generated numbers\n2. Every Monday and Thursday the PowerBall rolls and 5 random winning numbers are displayed on our home page\n3. If 3 or more of your ticket numbers match with the winning numbers on the PowerBall, you win according to the Prizes listed\n4. To claim your prize, go to our home page and choose the 'Claim Prize' option"
-				response = response + "\nSelect from the following options:\n1. Pick your numbers\n2. Game Rules\n3. Claim your prize!\n4. Return to Homepage\n"
+				response = response + "\n\nSelect from the following options:\n1. Pick your numbers\n2. Game Rules\n3. Claim your prize!\n4. Return to Homepage\n"
 
 			elif choice == "3":
 				self.status = 3
@@ -41,27 +42,41 @@ class PowerBall:
 			else:
 				response = "Improper input."
 		elif self.status == 1:
-			response = "Do you want to choose your numbers? (Y)es or (N)o: "
-			"""
-			if (s == "Yes") or (s == "Y") or (s == "yes") or (s == "y"): 
-				lottery = input("Enter your numbers seperated by a comma: ")
-				tickets = lottery.split(",")
+			if choice.isdigit():
+				self.num = int(choice)
+				if self.num <= 0:
+					response = "Improper input."
+				else:
+					response = "Do you want to choose your numbers? (Y)es or (N)o: "
+
+			elif (choice == "Yes") or (choice == "Y") or (choice == "yes") or (choice == "y"): 
+				if self.num <= 0:
+					response = "Improper input."
+				else:
+					response = "Enter your numbers seperated by a comma: "
+
+			elif ',' in choice:
+				tickets = choice.split(",")
 				tickets = [int(i) for i in tickets]
-
-			else:
+				response = "Your tickets are: {}".format(tickets)
+			elif (choice == "No") or (choice == "N") or (choice == "no") or (choice == "n"):
 				tickets = game.GenerateRandom(num)
+				response = "Your tickets are: {}".format(tickets)
+			else:
+				response = "Improper input."
 
-			response = "Your tickets are: {}".format(tickets)
-			"""
 		elif self.status == 3:
-			num = choice
-			arr = num.split(",")
+
+			print("choice:"+choice)
+			arr = choice.split(",")
 			arr = [int(i) for i in arr]
 			prize = game.CalPrize(arr)
 			if prize != "":
 				response = "\nCongrats! You won {} BITPOINTS".format(prize)
 			else:
 				response = "\nSorry!, You didnt win anything"
+		else:
+			response = "Error status!!!"
 
 
 		print("ben:\n"+response)
