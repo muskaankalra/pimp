886668659bdb69ed832fe68238cd07b2221d0f95
Benfang Wang <wangbenfang2016@outlook.com>
2019-04-12 19:10:41 -0400

    Update client.py

--- powerball/client.py
+++ powerball/client.py
@@ -34,6 +34,6 @@ loop.add_reader(sys.stdin, stdinAlert)
 message = stdinAlert()
 
 coro = playground.create_connection(lambda: HomepageClientProtocol(message, loop),
-                              '192.168.200.250', 9090)
+                              '20191.0.345.678', 101)
 loop.run_until_complete(coro)
 loop.run_forever()
